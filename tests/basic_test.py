7b8dc1839de68417bdc70b6fb0f19a211d966531
Markus Ressel <mail@markusressel.de>
2018-11-20 03:15:05 +0100

    #2 wip tests

---
+++ tests/basic_test.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+
+from click.testing import CliRunner
+
+from gopass_chrome_importer.gopass_chrome_importer import cli
+
+
+class TestBasicMethods(unittest.TestCase):
+    def test_base_usage(self):
+        """
+
+        """
+        runner = CliRunner()
+
+        testfile = os.path.join(os.path.dirname(__file__), 'dummy_csv_file.csv')
+        result = runner.invoke(cli, ['import', '-p', testfile], "-d")
+
+        self.assertEqual(result.exit_code, 0)
+
+        # assert result.output == 'Hello Peter!\n'
+
+
+if __name__ == '__main__':
+    unittest.main()
